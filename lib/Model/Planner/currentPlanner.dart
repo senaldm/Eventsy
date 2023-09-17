066277a6a9282d97c82395217322a9478f302eca
DominicDiron <dirondemel99@gmail.com>
2023-09-17 10:49:28 +0530

    current user added

--- lib/Model/Planner/currentPlanner.dart
+++ lib/Model/Planner/currentPlanner.dart
@@ -1,14 +1,22 @@
 import 'dart:convert';
+import 'package:eventsy/model/currentId.dart';
 import 'package:http/http.dart' as http;
 
-class Planners {
-  String url = "http://127.0.0.1:8000/api/getCurrentPlanner/{id}";
-
+class CurrentPlanner {
+  
+  
   Future<List> getCurrentPlanner() async {
+
+    currentId currentuser = currentId();
+    int id = currentuser.currentUserId;
+    String url = "http://127.0.0.1:8000/api/getCurrentPlanner/$id";
+
+
     try {
       final response = await http.get(Uri.parse(url));
       if (response.statusCode == 200) {
-        return jsonDecode(response.body);
+        //print([jsonDecode(response.body)]);
+        return [jsonDecode(response.body)];
       } else {
         return Future.error('Server Error');
       }
